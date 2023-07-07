606bf003b630fc7ba905829d70ce2504d24811aa
Paulo <paulocorreapc93@gmail.com>
2023-07-07 12:30:53 -0400

    versão 1.0 da estilização css finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,26 @@ function App() {
   return (
     <div className="app">
       <div className="main">main</div>
-      <div className="pyramid">money</div>
+      <div className="pyramid">
+        <ul className="moneyList">
+          <li className="moneyListItem active">
+            <span className="moneyListItemNumber">4</span>
+            <span className="moneyListItemAmount">R$400</span>
+          </li>
+          <li className="moneyListItem">
+            <span className="moneyListItemNumber">3</span>
+            <span className="moneyListItemAmount">R$300</span>
+          </li>
+          <li className="moneyListItem">
+            <span className="moneyListItemNumber">2</span>
+            <span className="moneyListItemAmount">R$200</span>
+          </li>
+          <li className="moneyListItem">
+            <span className="moneyListItemNumber">1</span>
+            <span className="moneyListItemAmount">R$100</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
