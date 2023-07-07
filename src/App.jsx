be3a9d89aa616a9209cccc2adcd3ec2703d61627
Paulo <paulocorreapc93@gmail.com>
2023-07-07 12:40:18 -0400

    valores da lista 'moneyList' adicionados dinamicamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import './app.css'
+import "./app.css";
 
 function App() {
+  const moneyPyramid = [
+    { id: 1, amount: "R$100" },
+    { id: 2, amount: "R$200" },
+    { id: 3, amount: "R$300" },
+    { id: 4, amount: "R$500" },
+    { id: 5, amount: "R$1000" },
+    { id: 6, amount: "R$2000" },
+    { id: 7, amount: "R$4000" },
+    { id: 8, amount: "R$8000" },
+    { id: 9, amount: "R$16000" },
+    { id: 10, amount: "R$32000" },
+    { id: 11, amount: "R$64000" },
+    { id: 12, amount: "R$125000" },
+    { id: 13, amount: "R$250000" },
+    { id: 14, amount: "R$500000" },
+    { id: 15, amount: "R$1000000" },
+  ].reverse();
+
   return (
     <div className="app">
       <div className="main">main</div>
       <div className="pyramid">
         <ul className="moneyList">
-          <li className="moneyListItem active">
-            <span className="moneyListItemNumber">4</span>
-            <span className="moneyListItemAmount">R$400</span>
-          </li>
-          <li className="moneyListItem">
-            <span className="moneyListItemNumber">3</span>
-            <span className="moneyListItemAmount">R$300</span>
-          </li>
-          <li className="moneyListItem">
-            <span className="moneyListItemNumber">2</span>
-            <span className="moneyListItemAmount">R$200</span>
-          </li>
-          <li className="moneyListItem">
-            <span className="moneyListItemNumber">1</span>
-            <span className="moneyListItemAmount">R$100</span>
-          </li>
+          {moneyPyramid.map((m) => (
+            <li className="moneyListItem">
+              <span className="moneyListItemNumber">{m.id}</span>
+              <span className="moneyListItemAmount">{m.amount}</span>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
