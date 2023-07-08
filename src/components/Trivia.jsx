0fc354f1fcbac0e9396cb6045937eca28ae76fcf
Paulo <paulocorreapc93@gmail.com>
2023-07-08 11:32:46 -0400

    importação do componente 'trivia' em 'app.jsx'

---
+++ src/components/Trivia.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Trivia = () => {
+  return (
+    <div className='trivia'>
+        <div className="question">what's the best youtube channel</div>
+        <div className="answers">
+            <div className="answer">Lama Dev</div>
+            <div className="answer">Lama Dev</div>
+            <div className="answer">Lama Dev</div>
+            <div className="answer">Lama Dev</div>
+        </div>
+    </div>
+  )
+}
+
+export default Trivia
