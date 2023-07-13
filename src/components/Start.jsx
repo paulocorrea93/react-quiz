bffff733b307de67478ef837a86dfa25fe0792c4
Paulo <paulocorreapc93@gmail.com>
2023-07-13 12:59:44 -0400

    importado o hook 'useRef'

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,8 +1,12 @@
+import { useRef } from "react";
+
 export default function Start() {
+  const inputRef = useRef();
+
   return (
     <div className="start">
       <input placeholder="Enter your username" className="startInput" />
       <button className="startButton">Start</button>
     </div>
-  )
+  );
 }
