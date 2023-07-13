55670286ca8d7a05ddc79470f48eb2e6f00ca39a
Paulo <paulocorreapc93@gmail.com>
2023-07-13 13:04:24 -0400

    criada função para inserir o usuário e iniciar o quiz

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,12 +1,22 @@
 import { useRef } from "react";
 
-export default function Start() {
+export default function Start({ setUsername }) {
   const inputRef = useRef();
+
+  const handleClick = () => {
+    inputRef.current.value && setUsername(inputRef.current.value);
+  };
 
   return (
     <div className="start">
-      <input placeholder="Enter your username" className="startInput" />
-      <button className="startButton">Start</button>
+      <input
+        placeholder="Enter your username"
+        className="startInput"
+        ref={inputRef}
+      />
+      <button className="startButton" onClick={handleClick}>
+        Start
+      </button>
     </div>
   );
 }
