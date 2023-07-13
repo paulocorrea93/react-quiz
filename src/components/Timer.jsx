49895d004a935963c07dfa4261fd595ec4e60774
Paulo <paulocorreapc93@gmail.com>
2023-07-12 23:32:39 -0400

    adicionado estado de efeito para atualizar o tempo na tela a cada nova questão

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function Timer() {
+  const [timer, setTimer] = useState(30);
 
-    const [timer, setTimer] = useState(30)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer((prev) => prev - 1);
+    }, 1000);
+  });
 
-  return (
-    <div>
-      
-    </div>
-  )
+  return timer;
 }
